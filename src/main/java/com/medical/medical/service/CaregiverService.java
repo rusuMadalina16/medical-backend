ee33ad0fcc5576943e1184eb5b541a122aec9a77
unknown <rusumadalina16@gmail.com>
2020-10-25 19:08:08 +0200

    Caregiver start

--- src/main/java/com/medical/medical/service/CaregiverService.java
+++ src/main/java/com/medical/medical/service/CaregiverService.java
@@ -1,9 +1,12 @@
 package com.medical.medical.service;
 
+import com.medical.medical.dtos.CaregiverDto;
 import com.medical.medical.dtos.PatientEntityDto;
 
 import java.util.List;
 
 public interface CaregiverService {
     List<PatientEntityDto> getPatients(Long caregiverId);
+
+    CaregiverDto getCaregiverById(Long caregiverId);
 }
